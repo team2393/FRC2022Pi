d7d0c3ea7242ed2b787ef6d10fa0a1af659d61b9
Kay <ky9@ornl.gov>
2022-01-10 13:44:59 -0500

    Prep color info pipeline for re-use

--- src/main/java/ColorInfoPipeline.java
+++ src/main/java/ColorInfoPipeline.java
@@ -5,8 +5,6 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.wpi.cscore.CvSource;
@@ -23,23 +21,31 @@ import org.opencv.imgproc.Imgproc;
 /** Pipeline that provides info about color in center of image */
 public class ColorInfoPipeline implements VisionPipeline
 {
-    // Scaling factor for reduced size of processed image
+    /** Scaling factor for reduced size of processed image */
     public static final int scale = 2;
 
-    private final CvSource output;
-    private final int width, height, proc_width, proc_height;
+    protected final CvSource output;
+    protected final int width, height, proc_width, proc_height;
 
-    // Intermediate images used for processing
+    // Intermediate images used for pre-processing
     private final Mat small = new Mat(),
                       norm = new Mat(),
-                      blur = new Mat(),
-                      hsv = new Mat();
+                      blur = new Mat();
+    
+    /** HSV version of current frame */
+    protected final Mat hsv = new Mat();
 
-    // Counter for calls to `process()`
-    public AtomicInteger calls = new AtomicInteger();
+    /** RGB at center of image */
+    protected int center_b = 0, center_g = 0, center_r = 0;
 
-    // Colors for drawing overlay
-    private final Scalar overlay_bgr = new Scalar(200.0, 100.0, 255.0), contrast_bgr = new Scalar(0, 0, 0);
+    /** HSV at center of image */
+    protected int center_h = 0, center_s = 0, center_v = 0;
+
+    /** Counter for calls to `process()` */
+    protected AtomicInteger calls = new AtomicInteger();
+
+    /** Colors for drawing overlay */
+    protected final Scalar overlay_bgr = new Scalar(200.0, 100.0, 255.0), contrast_bgr = new Scalar(0, 0, 0);
 
     ColorInfoPipeline(final CvSource output, final int width, final int height)
     {
@@ -50,101 +56,122 @@ public class ColorInfoPipeline implements VisionPipeline
         proc_height = height / scale;
     }
 
-  @Override
-  public void process(final Mat frame)
-  {
-    // In principle, should be possible to re-use Mat()s:
-    // 1) Resize original frame to smaller tmp1
-    // 2) Normalize tmp1 into tmp2
-    // 3) Convert RGB from tmp2 into HLS tmp1
-    // .. but that resulted in strange values for HLS,
-    // like H > 180.
-    // So re-using Mats across process calls,
-    // but within one process call always using it for the
-    // same purpose.
+    /** Pre-process the frame
+     * 
+     *  Resize, normalixe, blur, convert to HSV
+     */
+    public void prepare(final Mat frame)
+    {
+        calls.incrementAndGet();
 
-    // Resize to use less CPU & memory to process 
-    Imgproc.resize(frame, small, new Size(proc_width, proc_height));
-    
-    // Scale colors to use full 0..255 range in case image was dark
-    Core.normalize(small, norm, 0.0, 255.0, Core.NORM_MINMAX);
+        // In principle, should be possible to re-use Mat()s:
+        // 1) Resize original frame to smaller tmp1
+        // 2) Normalize tmp1 into tmp2
+        // 3) Convert RGB from tmp2 into HLS tmp1
+        // .. but that resulted in strange values for HLS,
+        // like H > 180.
+        // So re-using Mats across process calls,
+        // but within one process call always using it for the
+        // same purpose.
 
-    // When moving the camera, or turning auto-focus off and de-focusing,
-    // we would detect the target, but when standing still and in perfect focus,
-    // we missed it?!
-    // --> Blurring the image helps detect the target!
-    Imgproc.blur(norm, blur, new Size(8, 8));
+        // Resize to use less CPU & memory to process 
+        Imgproc.resize(frame, small, new Size(proc_width, proc_height));
+        
+        // Scale colors to use full 0..255 range in case image was dark
+        Core.normalize(small, norm, 0.0, 255.0, Core.NORM_MINMAX);
 
-    // Convert to HSV
-    Imgproc.cvtColor(blur, hsv, Imgproc.COLOR_BGR2HSV);
+        // When moving the camera, or turning auto-focus off and de-focusing,
+        // we would detect the target, but when standing still and in perfect focus,
+        // we missed it?!
+        // --> Blurring the image helps detect the target!
+        Imgproc.blur(norm, blur, new Size(8, 8));
 
-    // Probe BGR and HSV at center of image,
-    int center_b = 0, center_g = 0, center_r = 0;
-    int center_h = 0, center_s = 0, center_v = 0;
-    // Average over 9 pixels at center x, y +-1
-    final byte[] probe = new byte[3];
-    int avg = 0;
-    for (int x=-1; x<=1; ++x)
-        for (int y=-1; y<=1; ++y)
-        {
-            norm.get(proc_height/2 + x, proc_width/2 + y, probe);
-            center_b += Byte.toUnsignedInt(probe[0]);
-            center_g += Byte.toUnsignedInt(probe[1]);
-            center_r += Byte.toUnsignedInt(probe[2]);
-            hsv.get(proc_height/2 + x, proc_width/2 + y, probe);
-            center_h += Byte.toUnsignedInt(probe[0]);
-            center_s += Byte.toUnsignedInt(probe[1]);
-            center_v += Byte.toUnsignedInt(probe[2]);
-            ++avg;
-        }
-    center_b /= avg;
-    center_g /= avg;
-    center_r /= avg;
-    center_h /= avg;
-    center_s /= avg;
-    center_v /= avg;
-    
-    // Show rect in center of image where pixel info is probed
-    Imgproc.rectangle(frame,
-                      new Point(width/2 - 2, height/2 - 2),
-                      new Point(width/2 + 2, height/2 + 2),
-                      overlay_bgr);
-    
-    // Show info at bottom of image.
-    // Paint it twice, overlay-on-black, for better contrast
-    SmartDashboard.putNumber("Center B", center_b);
-    SmartDashboard.putNumber("Center G", center_g);
-    SmartDashboard.putNumber("Center R", center_r);
-    SmartDashboard.putNumber("Center H", center_h);
-    SmartDashboard.putNumber("Center S", center_s);
-    SmartDashboard.putNumber("Center V", center_v);
-    final String info = String.format("# %3d RGB %3d %3d %3d HSV %3d %3d %3d",
-                                      calls.incrementAndGet(),
-                                      center_r,
-                                      center_g,
-                                      center_b,
-                                      center_h,
-                                      center_s,
-                                      center_v);
-    Imgproc.putText(frame,
-                    info,
-                    new Point(1, height-16),
-                    Core.FONT_HERSHEY_SIMPLEX,
-                    0.4,
-                    contrast_bgr,
-                    1);
-    Imgproc.putText(frame,
-                    info,
-                    new Point(2, height-15),
-                    Core.FONT_HERSHEY_SIMPLEX,
-                    0.4,
-                    overlay_bgr,
-                    1);
+        // Convert to HSV
+        Imgproc.cvtColor(blur, hsv, Imgproc.COLOR_BGR2HSV);
 
-    // Publish 'output'
-    // Typically show 'frame', i.e., original image with overlay.
-    // But could show 'blur' or any other intermediate image while debugging.
-    //output.putFrame(blur);
-    output.putFrame(frame);
-  }
+        // Probe BGR and HSV at center of image,
+        center_b = center_g = center_r = 0;
+        center_h = center_s = center_v = 0;
+
+        // Average over 9 pixels at center x, y +-1
+        final byte[] probe = new byte[3];
+        int avg = 0;
+        for (int x=-1; x<=1; ++x)
+            for (int y=-1; y<=1; ++y)
+            {
+                norm.get(proc_height/2 + x, proc_width/2 + y, probe);
+                center_b += Byte.toUnsignedInt(probe[0]);
+                center_g += Byte.toUnsignedInt(probe[1]);
+                center_r += Byte.toUnsignedInt(probe[2]);
+                hsv.get(proc_height/2 + x, proc_width/2 + y, probe);
+                center_h += Byte.toUnsignedInt(probe[0]);
+                center_s += Byte.toUnsignedInt(probe[1]);
+                center_v += Byte.toUnsignedInt(probe[2]);
+                ++avg;
+            }
+        center_b /= avg;
+        center_g /= avg;
+        center_r /= avg;
+        center_h /= avg;
+        center_s /= avg;
+        center_v /= avg;
+
+        SmartDashboard.putNumber("Center B", center_b);
+        SmartDashboard.putNumber("Center G", center_g);
+        SmartDashboard.putNumber("Center R", center_r);
+        SmartDashboard.putNumber("Center H", center_h);
+        SmartDashboard.putNumber("Center S", center_s);
+        SmartDashboard.putNumber("Center V", center_v);
+
+        // Show rect in center of image where pixel info is probed
+        Imgproc.rectangle(frame,
+                          new Point(width/2 - 2, height/2 - 2),
+                          new Point(width/2 + 2, height/2 + 2),
+                          overlay_bgr);
+    }
+
+    /** Show info at bottom of image.
+     *  @param frame Where to show the info
+     *  @param info Info text to show
+     */
+    protected void showInfo(final Mat frame, final String info)
+    {
+        // Paint it twice, overlay-on-black, for better contrast
+        Imgproc.putText(frame,
+                        info,
+                        new Point(1, height-16),
+                        Core.FONT_HERSHEY_SIMPLEX,
+                        0.4,
+                        contrast_bgr,
+                        1);
+        Imgproc.putText(frame,
+                        info,
+                        new Point(2, height-15),
+                        Core.FONT_HERSHEY_SIMPLEX,
+                        0.4,
+                        overlay_bgr,
+                        1);
+    }
+
+    @Override
+    public void process(final Mat frame)
+    {
+        prepare(frame);
+
+        final String info = String.format("# %3d RGB %3d %3d %3d HSV %3d %3d %3d",
+                                          calls.get(),
+                                          center_r,
+                                          center_g,
+                                          center_b,
+                                          center_h,
+                                          center_s,
+                                          center_v);
+        showInfo(frame, info);
+
+        // Publish 'output'
+        // Typically show 'frame', i.e., original image with overlay.
+        // But could show 'blur' or any other intermediate image while debugging.
+        //output.putFrame(blur);
+        output.putFrame(frame);
+      }
 }
